d8e1eb0539339cf57f1629780877d24f52dabec7
Volkan Aydingul <vaydingul20@ku.edu.tr>
2022-08-27 15:47:52 +0300

    Initialize the sensor module

--- carla_env/carla_env.py
+++ carla_env/carla_env.py
@@ -1,50 +1,91 @@
-from environment import Environment
-from modules.sensor.sensor import SensorModule
+from carla_env.environment import Environment
 
+# Import modules
+from carla_env.modules.server import server as s
+from carla_env.modules.client import client as c
+from carla_env.modules.actor import actor as a
+from carla_env.modules.vehicle import vehicle as v
+
+# Import utils
+import time
+import numpy as np
 class CarlaEnvironment(Environment):
 	"""Concrete implementation of Environment abstract base class"""
+
 	
 	def __init__(self, config):
 		"""Initialize the environment"""
-		super().__init__(config)
+		super().__init__()
+
 		self._set_default_config()
-		for k in config.keys():
-			self.config[k] = config[k]
-		self.reset()
+		if config is not None:
+			for k in config.keys():
+				self.config[k] = config[k]
+
+		# We have our server and client up and running
+		server = s.ServerModule(None)
+		server._start()
+
+		time.sleep(5.0)
+
+		client = c.ClientModule(None)
+		client._start()
+
+		self.spectator = client.world.get_spectator()
+
+		# Let's initialize a vehicle
+
+		vehicle = v.VehicleModule(
+			{"vehicle_model": "lincoln.mkz2017"}, client.client)
+
+		# Make this vehicle actor
+
+		actor = a.ActorModule({"actor": vehicle, "hero": True}, client.client)
+
+		spawn_points = client._get_spawn_points()
+		selected_spawn_point = np.random.choice(spawn_points)
+		actor._start(selected_spawn_point)
+		self.spectator.set_transform(selected_spawn_point)
 	
+
+		self.reset()
+
 	def reset(self):
 		"""Reset the environment"""
 		pass
-	
+
 	def step(self, action):
 		"""Perform an action in the environment"""
 		pass
-	
+
 	def render(self):
 		"""Render the environment"""
 		pass
-	
+
 	def close(self):
 		"""Close the environment"""
 		pass
-	
+
 	def seed(self, seed):
 		"""Set the seed for the environment"""
 		pass
-	
+
 	def get_state(self):
 		"""Get the state of the environment"""
 		pass
-	
+
 	def get_action_space(self):
 		"""Get the action space of the environment"""
 		pass
-	
+
 	def get_observation_space(self):
 		"""Get the observation space of the environment"""
 		pass
-	
+
 	def get_config(self):
 		"""Get the config of the environment"""
 		return self.config
 
+	def _set_default_config(self):
+		"""Set the default config of the environment"""
+		self.config = {}
--- carla_env/modules/sensor/sensor.py
+++ carla_env/modules/sensor/sensor.py
@@ -6,20 +6,73 @@ import carla
 
 class SensorModule(module.Module):
 	"""Concrete implementation of Module abstract base class for sensor management"""
-	def __init__(self, config) -> None:
+	def __init__(self, config, client) -> None:
 		super().__init__()
-		for k in config.keys():
-    self.config[k] = config[k]
+
+		if config is not None:
+			for k in config.keys():
+				self.config[k] = config[k]
 		self.sensor_dict = {}
-		self.client = None
-		self.world = None
-		self.map = None
+		self.client = client
+		self.world = self.client.get_world()
+		self.map = self.world.get_map()
+		
 	
 
+	def _start(self):
+		"""Start the sensor module"""
+		self.client = self.config["client"]
+		self.world = self.client.get_world()
+		self.map = self.world.get_map()
+		self.sensor_dict = self.config["sensor_dict"]
+		self.sensor_dict["client"] = self.client
+		self.sensor_dict["world"] = self.world
+		self.sensor_dict["map"] = self.map
+		self.sensor_dict["config"] = self.config
+		self.sensor_dict["sensor_module"] = self
+		for k in self.sensor_dict.keys():
+			self.sensor_dict[k]._start()
+		self.started = True
+
+	def _stop(self):
+		"""Stop the sensor module"""
+		for k in self.sensor_dict.keys():
+			self.sensor_dict[k]._stop()
+		self.started = False
+
+	def _get_sensor_data(self):
+		"""Get the sensor data"""
+		for k in self.sensor_dict.keys():
+			self.sensor_dict[k]._get_sensor_data()
+
+	def step(self):
+		"""Step the sensor"""
+		self._tick()
 
 
 
+	def reset(self):
+		"""Reset the sensor"""
+		pass
 
-if __name__ == "__main__":
+	def render(self):
+		"""Render the sensor"""
+		pass
+	
+	def close(self):
+		"""Close the sensor"""
+		pass
 
-	pass
+	def seed(self):
+		"""Seed the sensor"""
+		pass
+
+	def get_config(self):
+		"""Get the config of the sensor"""
+		return self.config
+	
+	def _set_default_config(self):
+		"""Set the default config of the sensor"""
+		self.config = {}
+
+
