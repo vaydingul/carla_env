616e142d951076b812cea06ed09defbfd50bae57
Volkan Aydingul <vaydingul20@ku.edu.tr>
2022-09-23 21:51:34 +0300

    Route visualizer is implemented

--- utils/misc/visualize_route.py
+++ utils/misc/visualize_route.py
@@ -9,11 +9,29 @@ GREEN = carla.Color(r=0, g=255, b=0)
 BLUE = carla.Color(r=0, g=0, b=255)
 MAGENTA = carla.Color(r=255, g=0, b=255)
 CYAN = carla.Color(r=0, g=255, b=255)
+YELLOW = carla.Color(r=255, g=255, b=0)
+BLACK = carla.Color(r=0, g=0, b=0)
+WHITE = carla.Color(r=255, g=255, b=255)
+GRAY = carla.Color(r=128, g=128, b=128)
+PINK = carla.Color(r=255, g=0, b=255)
 
+
+
+
+ROAD_OPTION_COLOR_MAP = {
+	
+	'LEFT': CYAN,
+	'RIGHT': MAGENTA,
+	'STRAIGHT': RED,
+	'LANEFOLLOW': GREEN,
+	'CHANGELANELEFT': YELLOW,
+	'CHANGELANERIGHT': BLUE
+
+}
 def calculate_route(world, start = None, end = None):
 	"""Calculate route between two points."""
 
-	dao = GlobalRoutePlannerDAO(world.get_map(), sampling_resolution=1.0)
+	dao = GlobalRoutePlannerDAO(world.get_map(), sampling_resolution=0.1)
 	grp = GlobalRoutePlanner(dao)
 	grp.setup()
 	route = grp.trace_route(start, end)
@@ -39,12 +57,26 @@ def visualize_route(world, start = None, end = None):
 
 	route = calculate_route(world, start, end)
 
-	for waypoint, _ in route:
-		#eewaypoint = map.get_waypoint(world.get_map(), point[0].location)
-		world.debug.draw_string(waypoint.transform.location, 'o', color=GREEN, life_time=10000)
-		offset_loc = waypoint.transform.location + carla.Location(z=0.5)
-		#world.debug.draw_string(offset_loc, str(waypoint.transform.location), color=RED, life_time=10000)
+	for ix, (waypoint, road_option) in enumerate(route):
+		print(f"Coordinate {ix}: {waypoint.transform.location}")
+		print(f"Road option {ix}: {road_option}")
 
+		if ix <= 10:
+		
+			world.debug.draw_string(waypoint.transform.location, 'o', color=WHITE, life_time=10000)
+
+		elif ix >= len(route) - 10:
+
+			world.debug.draw_string(waypoint.transform.location, 'o', color=BLACK, life_time=10000)
+			
+		else:
+
+			world.debug.draw_string(waypoint.transform.location, 'o', color=GRAY, life_time=10000)
+			
+
+		
+		#offset_loc = waypoint.transform.location + carla.Location(z=0.5)
+		#world.debug.draw_string(offset_loc, str(waypoint.transform.location), color=RED, life_time=10000)
 	return start, end
 
 def main(world = "Town06"):
