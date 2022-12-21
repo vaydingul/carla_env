0fc8bec6c0d97a47d18f1ab0227b9a327ecf86c0
Volkan Aydingul <vaydingul20@ku.edu.tr>
2022-12-21 23:06:40 +0300

    Batch size update

--- script/world_model_training/train_world_model_cluster_14_new_dataset.sh
+++ script/world_model_training/train_world_model_cluster_14_new_dataset.sh
@@ -13,7 +13,7 @@ echo "Conda environment is activated"
 python3 train_world_forward_model.py \
 	--lr=1e-4 \
 	--num_epochs=50 \
-	--batch_size=40 \
+	--batch_size=36 \
 	--num_workers=5 \
 	--data_path_train="/kuacc/users/vaydingul20/ground_truth_bev_model_train_data_4_town_02/" \
 	--data_path_val="/kuacc/users/vaydingul20/ground_truth_bev_model_val_data_4_town_02/" \
