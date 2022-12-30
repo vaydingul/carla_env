6ba2a3953e6a1d320c938fb543a02743dd9cd15e
Volkan Aydingul <vaydingul20@ku.edu.tr>
2022-12-30 12:14:37 +0300

    New run for start from beginning

--- script/policy_training_delta_target_w_occ_light_cost/train_dfm_km_cp_3.sh
+++ script/policy_training_delta_target_w_occ_light_cost/train_dfm_km_cp_3.sh
@@ -9,14 +9,16 @@ source activate carla
 echo "Conda environment is activated"
 
 # Run the world model
-python3 train_dfm_km_cp_delta_target_w_occ.py \
+python3 train_dfm_km_cp_delta_target_w_occ_ddp.py \
 	--lr=1e-4 \
 	--num_epochs=50 \
-	--batch_size=80 \
-	--num_workers=4 \
+	--batch_size=20 \
+	--num_workers=5 \
 	--data_path_train="/kuacc/users/vaydingul20/ground_truth_bev_model_train_data_4_town_02/" \
 	--data_path_val="/kuacc/users/vaydingul20/ground_truth_bev_model_val_data_4_town_02/" \
 	--resume=false \
+	--num_gpu=2 \
+	--master_port="12355" \
 	--lr_schedule=false \
 	--gradient_clip_type="norm" \
 	--gradient_clip_value=1.0 \
@@ -38,15 +40,15 @@ python3 train_dfm_km_cp_delta_target_w_occ.py \
 	--red_light_cost_weight=0.1 \
 	--pedestrian_cost_weight=0.000 \
 	--offroad_cost_weight=0.1 \
-	--action_mse_weight=0.0 \
+	--action_mse_weight=1.0 \
 	--action_jerk_weight=0.0 \
 	--target_mse_weight=0.0 \
 	--target_l1_weight=0.0 \
-	--ego_state_mse_weight=1.0 \
+	--ego_state_mse_weight=0.0 \
 	--wandb=true \
 	--wandb_project="mbl" \
 	--wandb_group="dfm-km-cp" \
-	--wandb_name="vanilla+ego_state_loss(binary_radar)(w/ speed)" \
+	--wandb_name="vanilla+bc(binary_radar)(new_world_model)" \
 	--ego_forward_model_path="pretrained_models/2022-09-30/17-49-06/ego_model_new.pt" \
-	--world_forward_model_wandb_link="vaydingul/mbl/1gftiw9w" \
+	--world_forward_model_wandb_link="vaydingul/mbl/2b2rklg9" \
 	--world_forward_model_checkpoint_number=49
