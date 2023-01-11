04fdba2cb8ef01e320eb6bdc2e2d2c4762d8c7c5
vaydingulWS <volkanaydingul@gmail.com>
2023-01-11 22:09:13 +0300

    Change seed

--- script/world_model_training_5Hz_extended_bev/train_world_model_cluster_4.sh
+++ script/world_model_training_5Hz_extended_bev/train_world_model_cluster_4.sh
@@ -11,10 +11,10 @@ echo "Conda environment is activated"
 
 # Run the world model
 python3 train_world_forward_model_ddp.py \
-	--seed=42 \
+	--seed=2023 \
 	--lr=1e-4 \
 	--num_epochs=100 \
-	--batch_size=120 \
+	--batch_size=150 \
 	--num_workers=4 \
 	--data_path_train="/kuacc/users/vaydingul20/ground_truth_bev_model_train_data_10Hz_multichannel_bev/" \
 	--data_path_val="/kuacc/users/vaydingul20/ground_truth_bev_model_val_data_10Hz_multichannel_bev/" \
