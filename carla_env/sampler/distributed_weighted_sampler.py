f6b71f6cd1703c385a8318841fdc090f9ce4db95
vaydingulWS <volkanaydingul@gmail.com>
2023-01-16 08:45:55 +0300

    Distributed weiighted sampler is implemented

---
+++ carla_env/sampler/distributed_weighted_sampler.py
@@ -0,0 +1,70 @@
+import torch
+import torch.distributed as dist
+from torch.utils.data import Sampler
+import math
+
+
+class DistributedWeightedSampler(Sampler):
+    def __init__(
+            self,
+            dataset,
+            weights,
+            num_replicas=None,
+            rank=None,
+            replacement=True):
+
+        if num_replicas is None:
+            if not dist.is_available():
+                raise RuntimeError(
+                    "Requires distributed package to be available")
+            num_replicas = dist.get_world_size()
+        if rank is None:
+            if not dist.is_available():
+                raise RuntimeError(
+                    "Requires distributed package to be available")
+            rank = dist.get_rank()
+
+        self.dataset = dataset
+        self.num_replicas = num_replicas
+        self.rank = rank
+        self.epoch = 0
+        self.num_samples = int(
+            math.ceil(len(self.dataset) * 1.0 / self.num_replicas))
+        self.total_size = self.num_samples * self.num_replicas
+        self.replacement = replacement
+        self.weights = weights
+
+    def __iter__(self):
+        # deterministically shuffle based on epoch
+        g = torch.Generator()
+        g.manual_seed(self.epoch)
+        if self.shuffle:
+            indices = torch.randperm(len(self.dataset), generator=g).tolist()
+        else:
+            indices = list(range(len(self.dataset)))
+
+        # add extra samples to make it evenly divisible
+        indices += indices[:(self.total_size - len(indices))]
+        assert len(indices) == self.total_size
+
+        # subsample
+        indices = indices[self.rank:self.total_size:self.num_replicas]
+        assert len(indices) == self.num_samples
+
+        # get targets (you can alternatively pass them in __init__, if this op
+        # is expensive)
+        targets = self.dataset.targets
+        targets = targets[self.rank:self.total_size:self.num_replicas]
+        assert len(targets) == self.num_samples
+
+        return iter(
+            torch.multinomial(
+                self.weights,
+                self.num_samples,
+                self.replacement).tolist())
+
+    def __len__(self):
+        return self.num_samples
+
+    def set_epoch(self, epoch):
+        self.epoch = epoch
