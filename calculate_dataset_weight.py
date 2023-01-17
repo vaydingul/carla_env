f502118e368c8079f19012baf78b78c951c43d5d
vaydingulWS <volkanaydingul@gmail.com>
2023-01-17 14:47:55 +0300

    Add tqdm

--- calculate_dataset_weight.py
+++ calculate_dataset_weight.py
@@ -1,9 +1,9 @@
 from carla_env.dataset.instance import InstanceDataset
-import os 
+import os
 import logging
 import torch
 import argparse
-
+from tqdm import tqdm
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     level=logging.INFO,
@@ -13,7 +13,7 @@ logging.basicConfig(
 
 def main(config):
 
-	dataset = InstanceDataset(
+    dataset = InstanceDataset(
         data_path=config.dataset_path,
         sequence_length=config.num_time_step_previous +
         config.num_time_step_future,
@@ -23,42 +23,42 @@ def main(config):
         bev_selected_channels=[0, 1, 2, 3, 4, 5, 6, 11],
         bev_calculate_offroad=False)
 
-	logger.info(f"Train dataset size: {len(dataset)}")
+    logger.info(f"Train dataset size: {len(dataset)}")
+
+    if os.path.exists(f"{config.dataset_path}/weights.pt"):
+        logger.info("Loading weights from file")
+        weights = torch.load(f"{config.dataset_path}/weights.pt")
+    else:
+        logger.info("Calculating weights")
+        weights = torch.Tensor(
+            [dataset.__getweight__(k) for k in tqdm(range(
+                len(dataset)))])
+        torch.save(weights, f"{config.dataset_path}/weights.pt")
 
 
-	if os.path.exists(f"{config.dataset_path}/weights.pt"):
-		logger.info("Loading weights from file")
-		weights = torch.load(f"{config.dataset_path}/weights.pt")
-	else:
-		logger.info("Calculating weights")
-		weights = torch.Tensor(
-			[dataset.__getweight__(k) for k in range(
-				len(dataset))])
-		torch.save(weights, f"{config.dataset_path}/weights.pt")
-    
 if __name__ == "__main__":
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument(
-		"--dataset_path",
-		type=str,
-		default="",
-		help="Path to the dataset")
-	parser.add_argument(
-		"--num_time_step_previous",
-		type=int,
-		default=5,
-		help="Number of time steps in the past")
-	parser.add_argument(
-		"--num_time_step_future",
-		type=int,
-		default=10,
-		help="Number of time steps in the future")
-	parser.add_argument(
-		"--dataset_dilation",
-		type=int,
-		default=2,
-		help="Dilation of the dataset")
-	config = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset_path",
+        type=str,
+        default="",
+        help="Path to the dataset")
+    parser.add_argument(
+        "--num_time_step_previous",
+        type=int,
+        default=5,
+        help="Number of time steps in the past")
+    parser.add_argument(
+        "--num_time_step_future",
+        type=int,
+        default=10,
+        help="Number of time steps in the future")
+    parser.add_argument(
+        "--dataset_dilation",
+        type=int,
+        default=2,
+        help="Dilation of the dataset")
+    config = parser.parse_args()
 
-	main(config)
+    main(config)
