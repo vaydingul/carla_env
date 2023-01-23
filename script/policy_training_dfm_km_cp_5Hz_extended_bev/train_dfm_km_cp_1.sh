d6f0f2144248c30acebec433fdc257d2ee5a0459
Volkan Aydingul <vaydingul20@ku.edu.tr>
2023-01-23 10:26:34 +0300

    New run script for policy

--- script/policy_training_dfm_km_cp_5Hz_extended_bev/train_dfm_km_cp_1.sh
+++ script/policy_training_dfm_km_cp_5Hz_extended_bev/train_dfm_km_cp_1.sh
@@ -8,22 +8,24 @@ source activate carla
 
 echo "Conda environment is activated"
 
+
+
 # Run the world model
 python3 train_dfm_km_cp_extended_bev.py \
 	--lr=1e-4 \
 	--num_epochs=50 \
-	--batch_size=20 \
-	--num_workers=5 \
-	--data_path_train="/kuacc/users/vaydingul20/ground_truth_bev_model_train_data_4_town_02/" \
-	--data_path_val="/kuacc/users/vaydingul20/ground_truth_bev_model_val_data_4_town_02/" \
+	--batch_size=100 \
+	--num_workers=4 \
+	--data_path_train="/kuacc/users/vaydingul20/ground_truth_bev_model_train_data_10Hz_multichannel_bev_dense_traffic/" \
+	--data_path_val="/kuacc/users/vaydingul20/ground_truth_bev_model_val_data_10Hz_multichannel_bev_dense_traffic/" \
 	--resume=false \
-	--num_gpu=2 \
+	--num_gpu=7 \
 	--master_port="12355" \
 	--lr_schedule=false \
 	--gradient_clip_type="norm" \
 	--gradient_clip_value=1.0 \
 	--dataset_dilation=2 \
-	--debug_render=false \
+	--debug_render=true \
 	--save_interval=5 \
 	--input_ego_location=1 \
 	--input_ego_yaw=1 \
@@ -34,24 +36,25 @@ python3 train_dfm_km_cp_extended_bev.py \
 	--action_size=2 \
 	--hidden_size=256 \
 	--num_layer=6 \
-	--road_cost_weight=-0.1 \
-	--road_on_cost_weight=-0.1 \
-	--road_off_cost_weight=0.1 \
-	--road_red_yellow_cost_weight=0.1 \
-	--road_green_cost_weight=-0.1 \
+	--dropout=0.0 \
+	--road_cost_weight=0.0 \
+	--road_on_cost_weight=0.0 \
+	--road_off_cost_weight=1.0 \
+	--road_red_yellow_cost_weight=1.0 \
+	--road_green_cost_weight=0.0 \
 	--lane_cost_weight=1.0 \
-	--vehicle_cost_weight=0.1 \
-	--offroad_cost_weight=0.1 \
+	--vehicle_cost_weight=1.0 \
+	--offroad_cost_weight=1.0 \
 	--action_mse_weight=1.0 \
 	--action_jerk_weight=0.0 \
-	--target_mse_weight=0.0 \
-	--target_l1_weight=0.0 \
-	--ego_state_mse_weight=0.0 \
+	--target_progress_weight=-1.0 \
+	--target_remainder_weight=1.0 \
+	--ego_state_mse_weight=1.0 \
 	--wandb=true \
 	--wandb_project="mbl" \
 	--wandb_group="dfm-km-cp-5Hz-extended-extended-bev" \
-	--wandb_name="vanilla+bc" \
+	--wandb_name="policy+bc+target+ego" \
 	--ego_forward_model_wandb_link="vaydingul/mbl/ssifa1go" \
 	--ego_forward_model_checkpoint_number=449 \
-	--world_forward_model_wandb_link="vaydingul/mbl/1jnw0xjn" \
-	--world_forward_model_checkpoint_number=49
+	--world_forward_model_wandb_link="vaydingul/mbl/2aed7ypg" \
+	--world_forward_model_checkpoint_number=95
