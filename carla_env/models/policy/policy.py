0cc59ede23e217cbcf64251207b3a12269562a63
vaydingulWS <volkanaydingul@gmail.com>
2023-02-11 21:59:52 +0300

    Refactor policy network

--- carla_env/models/policy/policy.py
+++ carla_env/models/policy/policy.py
@@ -2,49 +2,16 @@ from torch import nn
 import torch
 from carla_env.models.layers.encoder import Encoder, Encoder2D
 from utils.cost_utils import create_2x2_rotation_tensor_from_angle_tensor
+from utils.train_utils import organize_device
 
 
 class Policy(nn.Module):
-    def __init__(
-        self,
-        input_shape_world_state,
-        input_ego_location,
-        input_ego_yaw,
-        input_ego_speed,
-        action_size,
-        command_size=6,
-        target_location_size=2,
-        occupancy_size=None,
-        hidden_size=256,
-        layers=4,
-        delta_target=True,
-        single_world_state_input=False,
-        dropout=0.1,
-    ):
+    def __init__(self, config):
         super(Policy, self).__init__()
 
-        self.input_shape_world_state = input_shape_world_state
-
-        self.keys = []
-        if input_ego_location > 0:
-            self.keys.append("location")
-        if input_ego_yaw > 0:
-            self.keys.append("yaw")
-        if input_ego_speed > 0:
-            self.keys.append("speed")
-        self.input_shape_ego_state = (
-            input_ego_location * 2 + input_ego_yaw + input_ego_speed
-        )
-
-        self.action_size = action_size
-        self.command_size = command_size
-        self.target_location_size = target_location_size
-        self.occupancy_size = occupancy_size
-        self.hidden_size = hidden_size
-        self.layers = layers
-        self.delta_target = delta_target
-        self.single_world_state_input = single_world_state_input
-        self.dropout = dropout
+        self.set_default_config()
+        self.append_config(config)
+        self.build_from_config()
 
         self.world_state_encoder = Encoder2D(
             input_shape=self.input_shape_world_state,
@@ -65,21 +32,25 @@ class Policy(nn.Module):
             dropout=self.dropout,
         )
 
-        self.command_encoder = Encoder(
-            input_size=self.command_size,
-            output_size=self.hidden_size,
-            layers=self.layers,
-            dropout=self.dropout,
-        )
+        if self.use_command_encoder:
 
-        self.target_encoder = Encoder(
-            input_size=self.target_location_size,
-            output_size=self.hidden_size,
-            layers=self.layers,
-            dropout=self.dropout,
-        )
+            self.command_encoder = Encoder(
+                input_size=self.command_size,
+                output_size=self.hidden_size,
+                layers=self.layers,
+                dropout=self.dropout,
+            )
 
-        if self.occupancy_size is not None and self.occupancy_size > 0:
+        if self.use_target_encoder:
+
+            self.target_encoder = Encoder(
+                input_size=self.target_location_size,
+                output_size=self.hidden_size,
+                layers=self.layers,
+                dropout=self.dropout,
+            )
+
+        if self.use_occupancy_encoder:
 
             self.occupancy_encoder = Encoder(
                 input_size=self.occupancy_size,
@@ -104,16 +75,104 @@ class Policy(nn.Module):
                 nn.ReLU(),
             )
 
+        self.fc = nn.Sequential(
+            nn.Linear(
+                self.hidden_size
+                * (
+                    2
+                    + self.use_command_encoder
+                    + self.use_occupancy_encoder
+                    + self.use_target_encoder
+                ),
+                self.hidden_size * 2,
+            ),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size * 2, self.hidden_size),
+            nn.ReLU(),
+        )
+
         self.fc_acceleration = nn.Sequential(nn.Linear(self.hidden_size, 1), nn.Tanh())
         self.fc_steer = nn.Sequential(nn.Linear(self.hidden_size, 1), nn.Tanh())
 
-    def forward(self, ego_state, world_state, command, target_location, occupancy=None):
+    def build_from_config(self):
+
+        self.input_shape_world_state = self.config["input_shape_world_state"]
+
+        self.keys = []
+        if self.config["input_ego_location"] > 0:
+            self.keys.append("location")
+        if self.config["input_ego_yaw"] > 0:
+            self.keys.append("yaw")
+        if self.config["input_ego_speed"] > 0:
+            self.keys.append("speed")
+        self.input_shape_ego_state = (
+            self.config["input_ego_location"] * 2
+            + self.config["input_ego_yaw"]
+            + self.config["input_ego_speed"]
+        )
+
+        # The size of the output action prediction
+        # 2 for steer and acceleration
+        # 3 for steer, throttle and brake
+        self.action_size = self.config["action_size"]
+
+        # Whether to use command encoder in the architecture or not
+        self.use_command_encoder = self.config["use_command_encoder"]
+
+        # The size of the command input
+        self.command_size = self.config["command_size"]
+
+        # Whether to use target encoder in the architecture or not
+        self.use_target_encoder = self.config["use_target_encoder"]
+
+        # The size of the target location input
+        self.target_location_size = self.config["target_location_size"]
+
+        # Whether to use occupancy encoder in the architecture or not
+        self.use_occupancy_encoder = self.config["use_occupancy_encoder"]
+
+        # The size of the occupancy input
+        self.occupancy_size = self.config["occupancy_size"]
+
+        # The size of the hidden layer where everything is combined
+        self.hidden_size = self.config["hidden_size"]
+
+        # The number of layers in the encoder(s)
+        # If there are multiple encoders, the number of layers in each encoder is the same
+        self.layers = self.config["layers"]
+
+        # Whether to use target location as delta state or absolute state
+        self.delta_target = self.config["delta_target"]
+
+        # Whether to use single world state input or multiple world state inputs (history)
+        self.single_world_state_input = self.config["single_world_state_input"]
+
+        # Dropout rate
+        self.dropout = self.config["dropout"]
+
+    def forward(
+        self, ego_state, world_state, command=None, target_location=None, occupancy=None
+    ):
+
+        assert self.use_command_encoder == (command is not None), (
+            "Command encoder is used in the architecture, "
+            "but no command is provided or vice versa"
+        )
+        assert self.use_target_encoder == (target_location is not None), (
+            "Target encoder is used in the architecture, "
+            "but no target location is provided or vice versa"
+        )
+        assert self.use_occupancy_encoder == (occupancy is not None), (
+            "Occupancy encoder is used in the architecture, "
+            "but no occupancy is provided or vice versa"
+        )
 
         if self.single_world_state_input:
             world_state = world_state[:, -1:]
         world_state = world_state.view(
             world_state.shape[0], -1, world_state.shape[-2], world_state.shape[-1]
         )
+
         world_state_encoded = self.world_state_encoder(world_state)
 
         # Flatten the encoded world state
@@ -121,50 +180,44 @@ class Policy(nn.Module):
             world_state_encoded.view(world_state_encoded.size(0), -1)
         )
 
+        # Constitute the concatenated feature vector
+        x = torch.cat([world_state_encoded], dim=1)
+
+        # Encode the ego state
         ego_state_encoded = self.ego_state_encoder(
             torch.cat([ego_state[k] for k in self.keys], dim=1)
         )
 
-        command_encoded = self.command_encoder(command)
+        # Concatenate the ego state encoded vector to the feature vector
+        x = torch.cat([x, ego_state_encoded], dim=1)
 
-        if self.delta_target:
-            target_location = target_location - ego_state["location"]
-            # rot = create_2x2_rotation_tensor_from_angle_tensor(
-            #     ego_state["yaw"])
-            # target_location = torch.matmul(
-            #     rot, target_location.unsqueeze(-1)).squeeze(-1)
+        if self.use_command_encoder:
 
-        target_encoded = self.target_encoder(target_location)
+            command_encoded = self.command_encoder(command)
 
-        if (
-            occupancy is not None
-            and self.occupancy_size is not None
-            and self.occupancy_size > 0
-        ):
+            # Concatenate the command encoded vector to the feature vector
+            x = torch.cat([x, command_encoded], dim=1)
+
+        if self.use_target_encoder:
+
+            if self.delta_target:
+                target_location = target_location - ego_state["location"]
+                # rot = create_2x2_rotation_tensor_from_angle_tensor(
+                #     ego_state["yaw"])
+                # target_location = torch.matmul(
+                #     rot, target_location.unsqueeze(-1)).squeeze(-1)
+
+            target_encoded = self.target_encoder(target_location)
+
+            # Concatenate the target encoded vector to the feature vector
+            x = torch.cat([x, target_encoded], dim=1)
+
+        if self.use_occupancy_encoder:
 
             occupancy_encoded = self.occupancy_encoder(occupancy)
 
-            x = torch.cat(
-                (
-                    world_state_encoded,
-                    ego_state_encoded,
-                    command_encoded,
-                    target_encoded,
-                    occupancy_encoded,
-                ),
-                dim=1,
-            )
-        else:
-
-            x = torch.cat(
-                (
-                    world_state_encoded,
-                    ego_state_encoded,
-                    command_encoded,
-                    target_encoded,
-                ),
-                dim=1,
-            )
+            # Concatenate the occupancy encoded vector to the feature vector
+            x = torch.cat([x, occupancy_encoded], dim=1)
 
         x_encoded = self.fc(x)
         acceleration = self.fc_acceleration(x_encoded)
@@ -173,26 +226,37 @@ class Policy(nn.Module):
 
         return action
 
+    def set_default_config(self):
+
+        self.config = {
+            "input_shape_world_state": (8, 192, 192),
+            "input_ego_location": 2,
+            "input_ego_yaw": 1,
+            "input_ego_speed": 1,
+            "action_size": 2,
+            "use_command_encoder": True,
+            "command_size": 6,
+            "use_target_encoder": True,
+            "target_location_size": 2,
+            "use_occupancy_encoder": True,
+            "occupancy_size": 8,
+            "hidden_size": 256,
+            "layers": 4,
+            "delta_target": True,
+            "single_world_state_input": False,
+            "dropout": 0.1,
+        }
+
     @classmethod
-    def load_model_from_wandb_run(cls, run, checkpoint, device):
+    def load_model_from_wandb_run(cls, config, checkpoint_path, device):
 
         checkpoint = torch.load(
-            checkpoint.name,
-            map_location=f"cuda:{device}" if isinstance(device, int) else device,
+            checkpoint_path,
+            map_location=organize_device(device),
         )
 
-        model = cls(
-            input_shape_world_state=run.config["input_shape_world_state"],
-            input_ego_location=run.config["input_ego_location"],
-            input_ego_yaw=run.config["input_ego_yaw"],
-            input_ego_speed=run.config["input_ego_speed"],
-            action_size=run.config["action_size"],
-            hidden_size=run.config["hidden_size"],
-            occupancy_size=run.config["occupancy_size"],
-            layers=run.config["num_layer"],
-            delta_target=run.config["delta_target"],
-            single_world_state_input=run.config["single_world_state_input"],
-        )
+        model = cls(config)
+
         model.load_state_dict(checkpoint["model_state_dict"])
 
         return model
