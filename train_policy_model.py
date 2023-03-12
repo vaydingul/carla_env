6d07e8843430651477f74f78a862ec17338e3c10
vaydingulWS <volkanaydingul@gmail.com>
2023-03-12 19:28:19 +0300

    Fix wandb bug

--- train_policy_model.py
+++ train_policy_model.py
@@ -51,7 +51,7 @@ def main(rank, world_size, config):
     # ---------------------------------------------------------------------------- #
     #                                   WANDB RUN                                  #
     # ---------------------------------------------------------------------------- #
-    policy_model_run = create_wandb_run(config) if rank == 0 else None
+    policy_model_run = create_wandb_run(config)
 
     if config["wandb"]["resume"]:
         # Fetch the specific checkpoint from wandb cloud storage
