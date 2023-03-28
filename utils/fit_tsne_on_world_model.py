b3c9fa065b4aa6ff3d59c013bacedf47c938ecb3
vaydingulWS <volkanaydingul@gmail.com>
2023-03-29 00:41:34 +0300

    New config

--- utils/fit_tsne_on_world_model.py
+++ utils/fit_tsne_on_world_model.py
@@ -3,20 +3,44 @@ import argparse
 from datetime import datetime
 from pathlib import Path
 import logging
-import wandb
-
+from tqdm import tqdm
+import os 
 import torch
 from torch.utils.data import DataLoader, Subset
 
 from utils.train_utils import get_device, seed_everything
 from utils.model_utils import fetch_run_from_wandb_link, fetch_checkpoint_from_wandb_run
-from utils.path_utils import create_date_time_path
 from utils.config_utils import parse_yml
 from utils.factory import *
 from utils.wandb_utils import create_wandb_run
 from utils.log_utils import get_logger, configure_logger, pretty_print_config
-import wandb
 
+
+def plot_embeddings(embeddings, labels, name):
+
+    import matplotlib.pyplot as plt
+
+    # ---------------------------------------------------------------------------- #
+    #                                   PLOT                                       #
+    # ---------------------------------------------------------------------------- #
+
+    # Create the figure
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111)
+
+    # Plot the embeddings
+    ax.scatter(embeddings[:, 0], embeddings[:, 1], c=labels, cmap="jet")
+    ax.set_xlabel("TSNE 1")
+    ax.set_ylabel("TSNE 2")
+    ax.set_title("TSNE Embeddings")
+    # Add the colorbar
+    cbar = fig.colorbar(ax.collections[0])
+    
+    # Save the figure
+    fig.savefig(f"{name}.png")
+
+
+    
 
 def fit_tsne(
     model,
@@ -26,6 +50,7 @@ def fit_tsne(
     device="cpu",
     num_time_step_previous=20,
     num_time_step_future=10,
+    path = None, 
 ):
 
     # ---------------------------------------------------------------------------- #
@@ -35,20 +60,52 @@ def fit_tsne(
 
     # Get all the latent vectors
     latent_vectors = []
-    for batch in dataloader_test:
+    speeds = []
+    yaws = []
+    bev_channel_counts = []
+    for batch in tqdm(dataloader_test):
+        bev = batch["bev_world"]["bev"][:, :num_time_step_previous].to(device)
         latent_vectors.append(
-            model(batch["bev_world"]["bev"][:, :num_time_step_previous].to(device))
+            model(bev, encoded = True)
         )
+        speeds.append(
+            batch["ego"]["velocity_array"][:, :num_time_step_previous]
+            .norm(2, -1, keepdim=False)
+            .mean(1)
+            .to(device)
+        )
+        yaws.append(
+            batch["ego"]["rotation_array"][:, :num_time_step_previous, 2]
+            .mean(1)
+            .to(device)
+        )
+            
+        bev_channel_counts.append(
+            bev.sum([3, 4])
+            .mean(1)
+            .to(device)
+        )
+
     latent_vectors = torch.cat(latent_vectors, dim=0)
+    speeds = torch.cat(speeds, dim=0).cpu().detach().numpy()
+    yaws = torch.cat(yaws, dim=0).cpu().detach().numpy()
+    bev_channel_counts = torch.cat(bev_channel_counts, dim=0).cpu().detach().numpy()
 
     # Flatten the latent vectors
     latent_vectors = latent_vectors.view(latent_vectors.shape[0], -1)
 
+    logger.info(f"Latent vectors shape: {latent_vectors.shape}")
+
     # Fit the tsne
     embedded_latent_vectors = tsne.fit_transform(latent_vectors.cpu().detach().numpy())
 
-    return tsne
+    # Plot the embeddings
+    fig = plot_embeddings(embedded_latent_vectors, speeds, os.path.join(path, "speed.png"))
+    fig = plot_embeddings(embedded_latent_vectors, yaws, os.path.join(path, "yaw.png"))
+    for k in range(bev.shape[2]):
+        fig = plot_embeddings(embedded_latent_vectors, bev_channel_counts[:, k], os.path.join(path, f"bev_channel_{k}"))
 
+    
 
 def main(config):
 
@@ -115,7 +172,7 @@ def main(config):
     logger.info(f"Test dataloader size: {len(dataloader_test)}")
 
     # ---------------------------------------------------------------------------- #
-    #                               WORLD FORWARD MODEL                              #
+    #                               WORLD FORWARD MODEL                            #
     # ---------------------------------------------------------------------------- #
     model_class = world_forward_model_factory(run.config)
 
@@ -140,6 +197,9 @@ def main(config):
         **config["tsne"],
     )
 
+    path = os.path.join(config["save_path"], config["analysis"]["name"])
+    os.makedirs(path, exist_ok=True)
+    
     fit_tsne(
         model,
         dataloader_test,
@@ -148,8 +208,10 @@ def main(config):
         device=device,
         num_time_step_previous=config["num_time_step_previous"],
         num_time_step_future=config["num_time_step_future"],
+        path = path,
     )
 
+   
 
 if __name__ == "__main__":
 
@@ -157,7 +219,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--config_path",
         type=str,
-        default="/home/vaydingul/Documents/Codes/carla_env/configs/tsne/config.yml",
+        default="/home/volkan/Documents/Codes/carla_env/configs/tsne/config_extended_bev.yml",
     )
     args = parser.parse_args()
 
