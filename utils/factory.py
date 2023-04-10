5b2d391bf2bc749685dc478eb4abf1f020a570d6
vaydingulWS <volkanaydingul@gmail.com>
2023-04-10 19:42:34 +0300

    Dataset summarization tool implemented

--- utils/factory.py
+++ utils/factory.py
@@ -1,5 +1,4 @@
 def ego_forward_model_factory(config):
-
     if (
         (config["experiment_type"] == "train_ego_forward_model")
         or (config["experiment_type"] == "eval_ego_forward_model")
@@ -7,24 +6,19 @@ def ego_forward_model_factory(config):
         or (config["experiment_type"] == "test_dfm_km_cp")
         or (config["experiment_type"] == "test_mpc")
     ):
-
         if config["ego_forward_model"]["type"] == "KinematicBicycleModel":
-
             from carla_env.models.dynamic.vehicle import KinematicBicycleModel
 
             return KinematicBicycleModel
 
         else:
-
             raise ValueError("Invalid ego forward model type")
 
     else:
-
         raise ValueError("Invalid experiment type")
 
 
 def world_forward_model_factory(config):
-
     if (
         (config["experiment_type"] == "train_world_forward_model")
         or (config["experiment_type"] == "eval_world_forward_model")
@@ -34,75 +28,60 @@ def world_forward_model_factory(config):
         or (config["experiment_type"] == "test_mpc")
     ):
         if config["world_forward_model"]["type"] == "WorldBEVModel":
-
             from carla_env.models.world.world import WorldBEVModel
 
             return WorldBEVModel
 
         else:
-
             raise ValueError("Invalid world forward model type")
 
     else:
-
         raise ValueError("Invalid experiment type")
 
 
 def policy_model_factory(config):
-
     if (config["experiment_type"] == "train_policy_model") or (
         config["experiment_type"] == "test_policy_model"
     ):
-
         if config["policy_model"]["type"] == "CILRSPolicyModel":
-
             from carla_env.models.policy.policy_cilrs import Policy
 
             return Policy
 
         elif config["policy_model"]["type"] == "FusedPolicyModel":
-
             from carla_env.models.policy.policy_fused import Policy
 
             return Policy
 
         elif config["policy_model"]["type"] == "PolicyModel":
-
             from carla_env.models.policy.policy import Policy
 
             return Policy
 
         else:
-
             raise ValueError("Invalid policy model type")
 
     else:
-
         raise ValueError("Invalid experiment type")
 
 
 def cost_factory(config):
-
     if (
         (config["experiment_type"] == "train_policy_model")
         or (config["experiment_type"] == "test_policy_model")
         or (config["experiment_type"] == "test_mpc")
     ):
-
         if config["cost"]["type"] == "extended_bev":
-
             from carla_env.cost.masked_cost_batched_extended_bev import Cost
 
             return Cost
 
         elif config["cost"]["type"] == "simple_bev":
-
             from carla_env.cost.masked_cost_batched_simple_bev import Cost
 
             return Cost
 
         elif config["cost"]["type"] == "extended_bev_with_pedestrian":
-
             from carla_env.cost.masked_cost_batched_extended_bev_with_pedestrian import (
                 Cost,
             )
@@ -110,7 +89,6 @@ def cost_factory(config):
             return Cost
 
         elif config["cost"]["type"] == "simple_bev_with_pedestrian":
-
             from carla_env.cost.masked_cost_batched_simple_bev_with_pedestrian import (
                 Cost,
             )
@@ -118,20 +96,16 @@ def cost_factory(config):
             return Cost
 
         else:
-
             raise ValueError("Invalid bev_type")
 
     else:
-
         raise ValueError("Invalid experiment type")
 
 
 def model_factory(config):
-
     if (config["experiment_type"] == "train_policy_model") or (
         config["experiment_type"] == "test_dfm_km_cp"
     ):
-
         from carla_env.models.dfm_km_cp import (
             DecoupledForwardModelKinematicsCoupledPolicy,
         )
@@ -139,51 +113,41 @@ def model_factory(config):
         return DecoupledForwardModelKinematicsCoupledPolicy
 
     else:
-
         raise ValueError("Invalid experiment type")
 
 
 def trainer_factory(config):
-
     if config["experiment_type"] == "train_ego_forward_model":
-
         from carla_env.trainer.ego_forward_model import Trainer
 
         return Trainer
 
     elif config["experiment_type"] == "train_world_forward_model":
-
         from carla_env.trainer.world_forward_model import Trainer
 
         return Trainer
 
     elif config["experiment_type"] == "train_policy_model":
-
         from carla_env.trainer.policy_model import Trainer
 
         return Trainer
 
     else:
-
         raise ValueError("Invalid experiment type")
 
 
 def evaluator_factory(config):
-
     if config["experiment_type"] == "eval_ego_forward_model":
-
         from carla_env.evaluator.ego_forward_model import Evaluator
 
         return Evaluator
 
     elif config["experiment_type"] == "eval_world_forward_model":
-
         from carla_env.evaluator.world_forward_model import Evaluator
 
         return Evaluator
 
     elif config["experiment_type"] == "eval_world_forward_model_last_frame_repeated":
-
         from carla_env.evaluator.world_forward_model_last_frame_repeated import (
             Evaluator,
         )
@@ -191,244 +155,195 @@ def evaluator_factory(config):
         return Evaluator
 
     elif config["experiment_type"] == "eval_policy_model_leaderboard":
-
         from carla_env.evaluator.policy_model_leaderboard import Evaluator
 
         return Evaluator
 
     else:
-
         raise ValueError("Invalid experiment type")
 
 
 def tester_factory(config):
-
     if config["experiment_type"] == "test_policy_model":
-
         from carla_env.tester.policy_model import Tester
 
         return Tester
 
     elif config["experiment_type"] == "test_mpc":
-
         from carla_env.tester.mpc import Tester
 
         return Tester
 
     else:
-
         raise ValueError("Invalid experiment type")
 
 
 def optimizer_factory(config):
-
     if (
         (config["experiment_type"] == "train_policy_model")
         or (config["experiment_type"] == "train_world_forward_model")
         or (config["experiment_type"] == "train_ego_forward_model")
         or (config["experiment_type"] == "test_mpc")
     ):
-
         if config["training"]["optimizer"]["type"] == "Adam":
-
             from torch.optim import Adam
 
             return Adam
 
         elif config["training"]["optimizer"]["type"] == "SGD":
-
             from torch.optim import SGD
 
             return SGD
 
         elif config["training"]["optimizer"]["type"] == "RMSprop":
-
             from torch.optim import RMSprop
 
             return RMSprop
 
         elif config["training"]["optimizer"]["type"] == "AdamW":
-
             from torch.optim import AdamW
 
             return AdamW
 
         else:
-
             raise ValueError("Invalid optimizer_type")
 
     else:
-
         raise ValueError("Invalid experiment type")
 
 
 def loss_criterion_factory(config):
-
     if (
         (config["experiment_type"] == "train_policy_model")
         or (config["experiment_type"] == "train_world_forward_model")
         or (config["experiment_type"] == "train_ego_forward_model")
     ):
-
         if config["training"]["loss"]["criterion"] == "MSELoss":
-
             from torch.nn import MSELoss
 
             return MSELoss
 
         elif config["training"]["loss"]["criterion"] == "SmoothL1Loss":
-
             from torch.nn import SmoothL1Loss
 
             return SmoothL1Loss
 
         elif config["training"]["loss"]["criterion"] == "L1Loss":
-
             from torch.nn import L1Loss
 
             return L1Loss
 
         elif config["training"]["loss"]["criterion"] == "BCELoss":
-
             from torch.nn import BCELoss
 
             return BCELoss
 
         elif config["training"]["loss"]["criterion"] == "BCEWithLogitsLoss":
-
             from torch.nn import BCEWithLogitsLoss
 
             return BCEWithLogitsLoss
 
         else:
-
             raise ValueError("Invalid loss_criterion")
 
     else:
-
         raise ValueError("Invalid experiment type")
 
 
 def metric_factory(config):
-
     if (
         (config["experiment_type"] == "eval_world_forward_model")
         or (config["experiment_type"] == "eval_world_forward_model_last_frame_repeated")
         or (config["experiment_type"] == "eval_ego_forward_model")
     ):
-
         if config["evaluation"]["metric"]["type"] == "MSE":
-
             from torch.nn import MSELoss
 
             return MSELoss
 
         elif config["evaluation"]["metric"]["type"] == "SmoothL1Loss":
-
             from torch.nn import SmoothL1Loss
 
             return SmoothL1Loss
 
         elif config["evaluation"]["metric"]["type"] == "L1Loss":
-
             from torch.nn import L1Loss
 
             return L1Loss
 
         elif config["evaluation"]["metric"]["type"] == "BCELoss":
-
             from torch.nn import BCELoss
 
             return BCELoss
 
         elif config["evaluation"]["metric"]["type"] == "BCEWithLogitsLoss":
-
             from torch.nn import BCEWithLogitsLoss
 
             return BCEWithLogitsLoss
 
         elif config["evaluation"]["metric"]["type"] == "MAE":
-
             import torch
 
             def MAE(y_pred, y_true, kwargs):
-
                 return torch.mean(torch.abs(y_pred - y_true))
 
             return MAE
 
         else:
-
             raise ValueError("Invalid metric")
 
     else:
-
         raise ValueError("Invalid experiment type")
 
 
 def scheduler_factory(config):
-
     if (
         (config["experiment_type"] == "train_policy_model")
         or (config["experiment_type"] == "train_world_forward_model")
         or (config["experiment_type"] == "train_ego_forward_model")
     ):
-
         if config["training"]["scheduler"]["enable"]:
-
             if config["training"]["scheduler"]["type"] == "StepLR":
-
                 from torch.optim.lr_scheduler import StepLR
 
                 return StepLR
 
             elif config["training"]["scheduler"]["type"] == "ReduceLROnPlateau":
-
                 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
                 return ReduceLROnPlateau
 
             else:
-
                 raise ValueError("Invalid scheduler_type")
 
         else:
-
             return None
 
     else:
-
         raise ValueError("Invalid experiment type")
 
 
 def dataset_factory(config, class_name=None):
-
     if class_name is not None:
-
         if class_name == "InstanceDataset":
-
             from carla_env.dataset.instance import InstanceDataset
 
             return InstanceDataset
 
         elif class_name == "InstanceDatasetRAM":
-
             from carla_env.dataset.instance import InstanceDatasetRAM
 
             return InstanceDatasetRAM
 
         elif class_name == "TorchDataset":
-
             from carla_env.dataset.torch_dataset import TorchDataset
 
             return TorchDataset
 
         else:
-
             raise ValueError("Invalid class_name")
 
     else:
-
         if (
             (config["experiment_type"] == "train_ego_forward_model")
             or (config["experiment_type"] == "eval_ego_forward_model")
@@ -439,54 +354,45 @@ def dataset_factory(config, class_name=None):
                 config["experiment_type"]
                 == "eval_world_forward_model_last_frame_repeated"
             )
+            or (config["experiment_type"] == "dataset_summary")
         ):
-
             dataset_key = [key for key in config.keys() if "dataset" in key][0]
 
             if config[dataset_key]["type"] == "InstanceDataset":
-
                 from carla_env.dataset.instance import InstanceDataset
 
                 return InstanceDataset
 
             elif config[dataset_key]["type"] == "InstanceDatasetRAM":
-
                 from carla_env.dataset.instance import InstanceDatasetRAM
 
                 return InstanceDatasetRAM
 
             elif config[dataset_key]["type"] == "TorchDataset":
-
                 from carla_env.dataset.torch_dataset import TorchDataset
 
                 return TorchDataset
 
             else:
-
                 raise ValueError("Invalid dataset_type")
 
         else:
-
             raise ValueError("Invalid experiment type")
 
 
 def writer_factory(config):
-
     if (config["experiment_type"] == "collect_data_random") or (
         config["experiment_type"] == "collect_data_driving"
     ):
-
         from carla_env.writer.writer import InstanceWriter
 
         return InstanceWriter
 
     else:
-
         raise ValueError("Invalid experiment type")
 
 
 def writer_key_factory(config):
-
     if (config["experiment_type"] == "collect_data_random") or (
         config["experiment_type"] == "collect_data_driving"
     ):
@@ -495,25 +401,19 @@ def writer_key_factory(config):
         from carla_env.writer.writer import InstanceWriterType
 
         for key_ in config["writer"]["keys"]:
-
             if key_["type"] == "rgb_image":
-
                 type_ = InstanceWriterType.RGB_IMAGE
 
             elif key_["type"] == "bev_image":
-
                 type_ = InstanceWriterType.BEV_IMAGE
 
             elif key_["type"] == "json":
-
                 type_ = InstanceWriterType.JSON
 
             elif key_["type"] == "radar":
-
                 type_ = InstanceWriterType.RADAR
 
             else:
-
                 raise ValueError("Invalid writer_type")
 
             writer_type_list.append(
@@ -523,16 +423,13 @@ def writer_key_factory(config):
         return writer_type_list
 
     else:
-
         raise ValueError("Invalid experiment type")
 
 
 def environment_factory(config):
-
     if (config["experiment_type"] == "collect_data_random") or (
         config["experiment_type"] == "collect_data_driving"
     ):
-
         from carla_env.carla_env_data_collect import CarlaEnvironment
 
         return CarlaEnvironment
@@ -540,30 +437,25 @@ def environment_factory(config):
     elif (config["experiment_type"] == "test_policy_model") or (
         config["experiment_type"] == "test_mpc"
     ):
-
         from carla_env.carla_env_testing_traffic import CarlaEnvironment
 
         return CarlaEnvironment
 
     elif config["experiment_type"] == "eval_policy_model_leaderboard":
-
         from carla_env.carla_env_leaderboard_headless import CarlaEnvironment
 
         return CarlaEnvironment
 
     elif config["experiment_type"] == "play_carla":
-
         from carla_env.carla_env_playground import CarlaEnvironment
 
         return CarlaEnvironment
 
     else:
-
         raise ValueError("Invalid experiment type")
 
 
 def sensor_factory(config):
-
     if (
         (config["experiment_type"] == "collect_data_random")
         or (config["experiment_type"] == "collect_data_driving")
@@ -572,19 +464,15 @@ def sensor_factory(config):
         or (config["experiment_type"] == "play_carla")
         or (config["experiment_type"] == "eval_policy_model_leaderboard")
     ):
-
         sensors = config["environment"]["sensors"]
         sensor_list = []
         for sensor in sensors:
-
             if sensor["type"] == "RGBSensor":
-
                 from carla_env.modules.sensor.rgb_sensor import RGBSensorModule
 
                 cls = RGBSensorModule
 
             elif sensor["type"] == "CollisionSensor":
-
                 from carla_env.modules.sensor.collision_sensor import (
                     CollisionSensorModule,
                 )
@@ -592,13 +480,11 @@ def sensor_factory(config):
                 cls = CollisionSensorModule
 
             elif sensor["type"] == "VehicleSensor":
-
                 from carla_env.modules.sensor.vehicle_sensor import VehicleSensorModule
 
                 cls = VehicleSensorModule
 
             elif sensor["type"] == "SemanticSensor":
-
                 from carla_env.modules.sensor.semantic_sensor import (
                     SemanticSensorModule,
                 )
@@ -606,7 +492,6 @@ def sensor_factory(config):
                 cls = SemanticSensorModule
 
             elif sensor["type"] == "OccupancySensor":
-
                 from carla_env.modules.sensor.occupancy_sensor import (
                     OccupancySensorModule,
                 )
@@ -614,25 +499,21 @@ def sensor_factory(config):
                 cls = OccupancySensorModule
 
             elif sensor["type"] == "GNSSSensor":
-
                 from carla_env.modules.sensor.gnss_sensor import GNSSSensorModule
 
                 cls = GNSSSensorModule
 
             elif sensor["type"] == "IMUSensor":
-
                 from carla_env.modules.sensor.imu_sensor import IMUSensorModule
 
                 cls = IMUSensorModule
 
             elif sensor["type"] == "RadarSensor":
-
                 from carla_env.modules.sensor.radar_sensor import RadarSensorModule
 
                 cls = RadarSensorModule
 
             else:
-
                 raise ValueError("Invalid sensor_type")
 
             sensor_list.append(
@@ -642,12 +523,10 @@ def sensor_factory(config):
         return sensor_list
 
     else:
-
         raise ValueError("Invalid experiment type")
 
 
 def noiser_factory(config):
-
     if (
         (config["experiment_type"] == "collect_data_random")
         or (config["experiment_type"] == "collect_data_driving")
@@ -656,27 +535,21 @@ def noiser_factory(config):
         or (config["experiment_type"] == "play_carla")
         or (config["experiment_type"] == "eval_policy_model_leaderboard")
     ):
-
         noiser = config["environment"]["noiser"]
 
         if noiser["type"] == "DummyNoiser":
-
             from carla_env.modules.noiser.dummy import DummyNoiser
 
             return {"class": DummyNoiser, "config": {}}
 
         else:
-
             if noiser["type"] == "GaussianNoiser":
-
                 from carla_env.modules.noiser.gaussian import GaussianNoiser
 
                 return {"class": GaussianNoiser, "config": noiser["config"]}
 
             else:
-
                 raise ValueError("Invalid noiser_type")
 
     else:
-
         raise ValueError("Invalid experiment type")
--- utils/summarize_dataset.py
+++ utils/summarize_dataset.py
@@ -1,7 +1,8 @@
 import logging
 import argparse
 import numpy as np
-
+from tqdm import tqdm
+import os
 from utils.config_utils import parse_yml
 from utils.factory import *
 
@@ -15,7 +16,6 @@ logging.basicConfig(
 
 
 def main(config):
-
     # ---------------------------------------------------------------------------- #
     #                                 DATASET CLASS                                #
     # ---------------------------------------------------------------------------- #
@@ -27,18 +27,129 @@ def main(config):
     dataset = dataset_class(config["dataset"]["config"])
     logger.info(f"Dataset size: {len(dataset)}")
 
-    navigational_command_list = np.zeros((6,))
-    for i in range(len(dataset)):
-        # Get data
-        data = dataset[i]
-        navigational_command = data["navigation"]["command"]
-        navigational_command_list[int(navigational_command) - 1] += 1
+    if "bev_world" in dataset.read_keys:
+        bev_information = np.zeros((len(dataset), len(dataset.bev_selected_channels)))
 
-    print(navigational_command_list)
+    if "ego" in dataset.read_keys:
+        ego_rotation_information = np.zeros((len(dataset),))
+        ego_speed_information = np.zeros((len(dataset),))
+        ego_control_throttle_information = np.zeros((len(dataset),))
+        ego_control_steer_information = np.zeros((len(dataset),))
+        ego_control_brake_information = np.zeros((len(dataset),))
+
+    if "navigation" in dataset.read_keys:
+        navigation_command_information = np.zeros((len(dataset),))
+
+    if "navigation_downsampled" in dataset.read_keys:
+        navigation_downsampled_command_information = np.zeros((len(dataset),))
+
+    if "occ" in dataset.read_keys:
+        occ_information = np.zeros((len(dataset), 8))
+
+    navigational_command_list = np.zeros((6,))
+    for ix, k in enumerate(tqdm(range(0, len(dataset)))):
+        # Get data
+        data = dataset[k]
+
+        if "bev_world" in dataset.read_keys:
+            bev_information[ix] = data["bev_world"]["bev"].numpy()[0].sum(axis=(1, 2))
+
+        if "ego" in dataset.read_keys:
+            ego_rotation_information[ix] = data["ego"]["rotation_array"].numpy()[0][2]
+            ego_speed_information[ix] = np.linalg.norm(
+                data["ego"]["velocity_array"].numpy()[0]
+            )
+            ego_control_throttle_information[ix] = data["ego"]["control_array"].numpy()[
+                0
+            ][0]
+            ego_control_steer_information[ix] = data["ego"]["control_array"].numpy()[0][
+                1
+            ]
+            ego_control_brake_information[ix] = data["ego"]["control_array"].numpy()[0][
+                2
+            ]
+
+        if "navigation" in dataset.read_keys:
+            navigation_command_information[ix] = (
+                data["navigation"]["command"].numpy()[0] - 1
+            )
+
+        if "navigation_downsampled" in dataset.read_keys:
+            navigation_downsampled_command_information[ix] = (
+                data["navigation_downsampled"]["command"].numpy()[0] - 1
+            )
+
+        if "occ" in dataset.read_keys:
+            occ_information[ix] = data["occ"]["occupancy"].numpy()[0]
+
+    dataset_information = {}
+
+    if "bev_world" in dataset.read_keys:
+        for k in range(len(dataset.bev_selected_channels)):
+            histogram, bins = np.histogram(bev_information[:, k], bins=100)
+            dataset_information[f"bev_world_{k}"] = {
+                "histogram": histogram,
+                "bins": bins,
+            }
+
+    if "ego" in dataset.read_keys:
+        histogram, bins = np.histogram(ego_rotation_information, bins=100)
+        dataset_information["ego_rotation"] = {"histogram": histogram, "bins": bins}
+
+        histogram, bins = np.histogram(ego_speed_information, bins=100)
+        dataset_information["ego_speed"] = {"histogram": histogram, "bins": bins}
+
+        histogram, bins = np.histogram(ego_control_throttle_information, bins=100)
+        dataset_information["ego_control_throttle"] = {
+            "histogram": histogram,
+            "bins": bins,
+        }
+
+        histogram, bins = np.histogram(ego_control_steer_information, bins=100)
+        dataset_information["ego_control_steer"] = {
+            "histogram": histogram,
+            "bins": bins,
+        }
+
+        histogram, bins = np.histogram(ego_control_brake_information, bins=100)
+        dataset_information["ego_control_brake"] = {
+            "histogram": histogram,
+            "bins": bins,
+        }
+
+    if "navigation" in dataset.read_keys:
+        histogram, bins = np.histogram(navigation_command_information, bins=100)
+        dataset_information["navigation_command"] = {
+            "histogram": histogram,
+            "bins": bins,
+        }
+
+    if "navigation_downsampled" in dataset.read_keys:
+        histogram, bins = np.histogram(
+            navigation_downsampled_command_information, bins=100
+        )
+        dataset_information["navigation_downsampled_command"] = {
+            "histogram": histogram,
+            "bins": bins,
+        }
+
+    if "occ" in dataset.read_keys:
+        for k in range(8):
+            histogram, bins = np.histogram(occ_information[:, k], bins=100)
+            dataset_information[f"occ_{k}"] = {"histogram": histogram, "bins": bins}
+
+    # ---------------------------------------------------------------------------- #
+
+    # ---------------------------------------------------------------------------- #
+    #                                 SAVE DATASET                                 #
+    # ---------------------------------------------------------------------------- #
+    save_path = os.path.join(
+        config["dataset"]["config"]["data_path"], f"{'_'.join(dataset.read_keys)}.npz"
+    )
+    np.savez(save_path, dataset_information)
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--config_path", type=str, default="configs/dataset_summary/config.yml"
